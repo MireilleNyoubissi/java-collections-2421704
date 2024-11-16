d92ff331e0faf6b37456d3dc1a46a3df7c34dac9
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-11-16 14:18:31 +0000

    Creating collections

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,10 +1,23 @@
 package com.linkedin.collections;
+import java.util.collection;
+import java.util.Arraylist;
+import java.util.HashSet;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
-		
+		Collection<String> c = new ArrayList<>();
+		c.add("snowboard");
+		c.add("books");
+		c.add("linux");
+		c.add("swoaboard");
+
+		Collection<String> favouriteThings = new HashSet<>(c);
+
+		System.out.println(favouriteThings);
+
+
 	}
 
 }

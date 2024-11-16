616611f066cf88f4f44efe05aecf91a759ef8165
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-11-16 12:45:49 +0000

    working with collections using interfaces

--- src/main/java/com/linkedin/collections/Implementation.java
+++ src/main/java/com/linkedin/collections/Implementation.java
@@ -1,6 +1,16 @@
 package com.linkedin.collections;
 
-public class Implementation implements Contract  {
+public class Implementation implements Contract {
 
+    public void term1() {
+        System.out.println("Implementation:term1");
+    }
 
+    public void term2() {
+        System.out.println("Implementation:term2");
+    }
+
+    public void extendedTerm() {
+        System.out.println("Implementation:Extended");
+    }
 }

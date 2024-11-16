64ed7e810715ef4ccdaab82e740af6a55d48f705
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-11-16 11:37:54 +0000

    improving code using collections

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -6,13 +6,19 @@ public class Application {
 	
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.0);
+		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.0);
+		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
 
-		double total = getPotentialRevenue(cambridge, manchester);
+		Collection<Room> rooms = List.of(cambridge, manchester, piccadilly);
+		double total = getPotentialRevenue(rooms);
 		System.out.println(total);
 	}
 	
-	private static double getPotentialRevenue(Room room1, Room room2) {
-		return room1.getRate() + room2.getRate();
+	private static double getPotentialRevenue(Collection<Room> rooms) {
+
+		return rooms.stream()
+				.mapToDouble(r -> r.getRate())
+				.sum();
 	}
 
 }
